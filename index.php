b48479317506c23e87b8e77912efdec12858f542
CarloSalaris <carloa.salaris@gmail.com>
2023-06-20 16:10:25 +0200

    HTML + bootstrap + Each hotel in a div

--- index.php
+++ index.php
@@ -1,51 +1,85 @@
-<?php
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <!-- BOOTSTRAP -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-KK94CHFLLe+nY2dmCWGMq91rCGa5gtU4mk92HdvYe+M/SXH301p5ILy+dN9+nJOZ" crossorigin="anonymous">
+    <!-- CSS -->
+    <style>
 
-//Array of hotels
-$hotels = [
+        /*************
+        GENERAL FORMAT
+        **************/
 
-    [
-        'name' => 'Hotel Belvedere',
-        'description' => 'Hotel Belvedere Descrizione',
-        'parking' => true,
-        'vote' => 4,
-        'distance_to_center' => 10.4
-    ],
-    [
-        'name' => 'Hotel Futuro',
-        'description' => 'Hotel Futuro Descrizione',
-        'parking' => true,
-        'vote' => 2,
-        'distance_to_center' => 2
-    ],
-    [
-        'name' => 'Hotel Rivamare',
-        'description' => 'Hotel Rivamare Descrizione',
-        'parking' => false,
-        'vote' => 1,
-        'distance_to_center' => 1
-    ],
-    [
-        'name' => 'Hotel Bellavista',
-        'description' => 'Hotel Bellavista Descrizione',
-        'parking' => false,
-        'vote' => 5,
-        'distance_to_center' => 5.5
-    ],
-    [
-        'name' => 'Hotel Milano',
-        'description' => 'Hotel Milano Descrizione',
-        'parking' => true,
-        'vote' => 2,
-        'distance_to_center' => 50
-    ],
+        * {
+            margin: 0; 
+            padding: 0;
+            box-sizing: border-box;
+        }
 
-];
+    </style>
 
-//for each hotel in $hotels print all informations
-foreach ($hotels as $hotel) {
-    echo "<pre>";
-    var_dump($hotel);
-    echo "</pre>";
-}
+    <title>PHP hotels</title>
+</head>
+<body>
+    
+    <!-- Array of hotels -->
+    <?php
+    $hotels = [
+    
+        [
+            'name' => 'Hotel Belvedere',
+            'description' => 'Hotel Belvedere Descrizione',
+            'parking' => true,
+            'vote' => 4,
+            'distance_to_center' => 10.4
+        ],
+        [
+            'name' => 'Hotel Futuro',
+            'description' => 'Hotel Futuro Descrizione',
+            'parking' => true,
+            'vote' => 2,
+            'distance_to_center' => 2
+        ],
+        [
+            'name' => 'Hotel Rivamare',
+            'description' => 'Hotel Rivamare Descrizione',
+            'parking' => false,
+            'vote' => 1,
+            'distance_to_center' => 1
+        ],
+        [
+            'name' => 'Hotel Bellavista',
+            'description' => 'Hotel Bellavista Descrizione',
+            'parking' => false,
+            'vote' => 5,
+            'distance_to_center' => 5.5
+        ],
+        [
+            'name' => 'Hotel Milano',
+            'description' => 'Hotel Milano Descrizione',
+            'parking' => true,
+            'vote' => 2,
+            'distance_to_center' => 50
+        ],  
+    ];
+    ?>
 
-?>
+<div class="container my-5">
+    <h1 class="text-center">LIST OF HOTELS</h1>
+    
+    <?php
+    //for each hotel in $hotels print all informations
+    foreach ($hotels as $hotel) {
+        echo "<div>";
+            echo "<pre>";
+            var_dump($hotel);
+            echo "</pre>";
+        echo "</div>";
+    }
+    ?>
+</div>
+    
+</body>
+</html>
