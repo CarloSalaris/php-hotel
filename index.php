417ad99563d814fea2ba63fa31fdb53b83656e28
CarloSalaris <carloa.salaris@gmail.com>
2023-06-20 15:32:26 +0200

    Print each hotel's info from array with var_dump

---
+++ index.php
@@ -0,0 +1,51 @@
+<?php
+
+//Array of hotels
+$hotels = [
+
+    [
+        'name' => 'Hotel Belvedere',
+        'description' => 'Hotel Belvedere Descrizione',
+        'parking' => true,
+        'vote' => 4,
+        'distance_to_center' => 10.4
+    ],
+    [
+        'name' => 'Hotel Futuro',
+        'description' => 'Hotel Futuro Descrizione',
+        'parking' => true,
+        'vote' => 2,
+        'distance_to_center' => 2
+    ],
+    [
+        'name' => 'Hotel Rivamare',
+        'description' => 'Hotel Rivamare Descrizione',
+        'parking' => false,
+        'vote' => 1,
+        'distance_to_center' => 1
+    ],
+    [
+        'name' => 'Hotel Bellavista',
+        'description' => 'Hotel Bellavista Descrizione',
+        'parking' => false,
+        'vote' => 5,
+        'distance_to_center' => 5.5
+    ],
+    [
+        'name' => 'Hotel Milano',
+        'description' => 'Hotel Milano Descrizione',
+        'parking' => true,
+        'vote' => 2,
+        'distance_to_center' => 50
+    ],
+
+];
+
+//for each hotel in $hotels print all informations
+foreach ($hotels as $hotel) {
+    echo "<pre>";
+    var_dump($hotel);
+    echo "</pre>";
+}
+
+?>
